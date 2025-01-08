a2654e309a6ff2976e6697ab484eeac732f6801b
Kevin Nguyen <kevin.nguyen2321@yahoo.com>
2025-01-08 10:34:03 -0600

    Add new controller for category

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using Tabloid.Models;
+using Tabloid.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace Tabloid.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CategoryController : ControllerBase
+{
+    private TabloidDbContext _dbContext;
+
+    public CategoryController(TabloidDbContext context)
+    {
+        _dbContext = context;
+    }
+
+    [HttpGet]
+    // [Authorize]
+    public IActionResult Get()
+    {
+        return Ok(_dbContext.Categories
+        .OrderBy(c => c.Name)
+        .ToList());
+    }
+
+    
+}
